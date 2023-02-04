f4561f79647e0970262d9e8995ea6b54503e823e
Lars Robeerst <larsrobeerst1@gmail.com>
2023-02-04 15:13:33 +0100

    DS-opdracht-4.1.4 (#5)

--- relational-data-storage/opdracht-4/DS-opdracht-4.1.3.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.1.3.sql
@@ -1,4 +1,4 @@
-SELECT mhl.mhl_suppliers.name, mhl.mhl_suppliers.straat, mhl.mhl_suppliers.huisnr, mhl.mhl_suppliers.postcode
+SELECT mhl.mhl_rubrieken.name as R_name, mhl.mhl_suppliers.name as S_name, mhl.mhl_suppliers.straat, mhl.mhl_suppliers.huisnr, mhl.mhl_suppliers.postcode
 FROM mhl.mhl_suppliers
 JOIN mhl.mhl_suppliers_mhl_rubriek_view ON  mhl.mhl_suppliers_mhl_rubriek_view.mhl_suppliers_ID = mhl.mhl_suppliers.ID
 JOIN mhl.mhl_rubrieken ON  mhl.mhl_rubrieken.ID = Mhl.mhl_suppliers_mhl_rubriek_view.mhl_rubriek_view_ID
