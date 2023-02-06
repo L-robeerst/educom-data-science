57890d9765b52b4ab1b16e03077f2f261c608674
Lars Robeerst <larsrobeerst1@gmail.com>
2023-02-06 17:59:03 +0100

    DS-opdracht-4.2.4 correctie (#5)

--- relational-data-storage/opdracht-4/DS-opdracht-4.2.4.sql
+++ relational-data-storage/opdracht-4/DS-opdracht-4.2.4.sql
@@ -1,4 +1,5 @@
-SELECT S.ID, IFNULL(H.name, S.name) AS hoofdrubriek, IF(ISNULL(H.name), '', S.name) AS subrubriek
-FROM mhl.mhl_rubrieken S
-left JOIN mhl.mhl_rubrieken H ON S.parent = H.id
-ORDER BY hoofdrubriek, subrubriek
+SELECT mhl.mhl_suppliers.name, mhl.mhl_propertytypes.name, IFNULL(mhl.mhl_yn_properties.content, "NOT SET") as value
+FROM mhl.mhl_suppliers
+CROSS JOIN mhl.mhl_propertytypes
+LEFT JOIN mhl.mhl_yn_properties ON mhl.mhl_suppliers.id = mhl.mhl_yn_properties.supplier_ID and mhl.mhl_propertytypes.id = mhl.mhl_yn_properties.propertytype_ID
+WHERE mhl.mhl_suppliers.city_ID = 104 AND mhl.mhl_propertytypes.proptype = "A";
